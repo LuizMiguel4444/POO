cb157bb664b64e2767f8fa39b82a0740ca940739
luizmiguel4444 <luizmiguel4444@gmail.com>
2024-01-23 23:47:26 -0300

    feat: fiz até o exercício 3

--- Receita 1/lib/main.dart
+++ Receita 1/lib/main.dart
@@ -2,20 +2,82 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.deepPurple),
+    theme: ThemeData(primarySwatch: Colors.green),
     home: Scaffold(
-      appBar: AppBar(title: Text("Meu app")
-    ),
+      appBar: AppBar(title: Text(
+        "Meu primeiro app",
+        style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 30.0,
+          fontWeight: FontWeight.normal,
+        )
+      )),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Apenas começando..."),
-            Text("No meio..."),
-            Text("Terminando...")              
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Receita 1',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "By",
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+              )   
+            ),
+            Text(
+              "Luiz Miguel",
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+              )
+            ),
           ]
-        )          
+        )
       ),
-      bottomNavigationBar: Text("Botão 1"),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "Botão 1",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 20.0,
+              fontWeight: FontWeight.normal,
+            )
+          ),
+          Text(
+            "Botão 2",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 20.0,
+              fontWeight: FontWeight.normal,
+            )
+          ),
+          Text(
+            "Botão 3",
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 20.0,
+              fontWeight: FontWeight.normal,
+            )
+          ),
+        ]
+      )
     )
   );
   runApp(app);
