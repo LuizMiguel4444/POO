c40d7005f7580dc6bcc2c2f3bd218b12c4373e5f
luizmiguel4444 <luizmiguel4444@gmail.com>
2024-01-23 22:35:26 -0300

    feat: iniciei a primeira receita

---
+++ Receita 1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp app = MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.deepPurple),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Meu app")
+    ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Apenas começando..."),
+            Text("No meio..."),
+            Text("Terminando...")              
+          ]
+        )          
+      ),
+      bottomNavigationBar: Text("Botão 1"),
+    )
+  );
+  runApp(app);
+}
